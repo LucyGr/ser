0fe04c1240d7187c29d56dd5c6f77b00df19a4f5
Lucy Greenwood <lucygreenwood@ndmpc967.ndm.ox.ac.uk>
2024-02-20 14:08:09 +0000

    adding hyperparameters as inputs

--- bin/cli.py
+++ bin/cli.py
@@ -19,14 +19,21 @@ def train(
     name: str = typer.Option(
         ..., "-n", "--name", help="Name of experiment to save under."
     ),
+    epochs: int = typer.Option(
+        2, "-e", "--epoch", help="Number of epochs."
+    ),
+    batch_size: int = typer.Option(
+        1000, "-bs", "--batch_size", help="Batch size."
+    ),
+    learning_rate: float = typer.Option(
+        0.01, "-lr", "--learning_rate", help="Learning rate."
+    ),
 ):
     print(f"Running experiment {name}")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    epochs = 2
-    batch_size = 1000
-    learning_rate = 0.01
 
     # save the parameters!
+    #######################
 
     # load model
     model = Net().to(device)
