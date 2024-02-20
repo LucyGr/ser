20e2383019e3ad65ddcd4217f0c8b8260ec2ca25
Lucy Greenwood <lucygreenwood@ndmpc967.ndm.ox.ac.uk>
2024-02-20 15:25:53 +0000

    params could be a problem

--- bin/cli.py
+++ bin/cli.py
@@ -3,9 +3,10 @@ import torch
 from torch import optim
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+from torchvision import transforms
 from ser.data import train_dataloader, validate_dataloader
+from ser.train import train as trainer
+
 
 import typer
 
@@ -35,12 +36,7 @@ def train(
 
     # save the parameters!
     ####################### need to do this #############
-
-    # load model
-    model = Net().to(device)
-
-    # setup params
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    params = (name, epochs, batch_size, learning_rate)
 
     # torch transforms
     ts = transforms.Compose(
@@ -50,41 +46,11 @@ def train(
 # data loaders were here now in data.py
     training_dataloader = train_dataloader(batch_size, ts)
     validation_dataloader = validate_dataloader(batch_size, ts)
-
+    
+    save_here = PROJECT_ROOT / "saved stuff"
     # train
-    for epoch in range(epochs):
-        for i, (images, labels) in enumerate(training_dataloader):
-            images, labels = images.to(device), labels.to(device)
-            model.train()
-            optimizer.zero_grad()
-            output = model(images)
-            loss = F.nll_loss(output, labels)
-            loss.backward()
-            optimizer.step()
-            print(
-                f"Train Epoch: {epoch} | Batch: {i}/{len(training_dataloader)} "
-                f"| Loss: {loss.item():.4f}"
-            )
-            # validate
-            val_loss = 0
-            correct = 0
-            with torch.no_grad():
-                for images, labels in validation_dataloader:
-                    images, labels = images.to(device), labels.to(device)
-                    model.eval()
-                    output = model(images)
-                    val_loss += F.nll_loss(output, labels, reduction="sum").item()
-                    pred = output.argmax(dim=1, keepdim=True)
-                    correct += pred.eq(labels.view_as(pred)).sum().item()
-                val_loss /= len(validation_dataloader.dataset)
-                val_acc = correct / len(validation_dataloader.dataset)
+    trainer(save_here, params, training_dataloader, validation_dataloader,device )
 
-                print(
-                    f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
-                )
-
-
-### model was here
 
 
 @main.command()
