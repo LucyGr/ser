a986e8d86bb42928df2f38cbe30761bcade72c4d
Lucy Greenwood <lucygreenwood@ndmpc967.ndm.ox.ac.uk>
2024-02-21 11:04:13 +0000

    starting to move things across

--- bin/cli.py
+++ bin/cli.py
@@ -11,6 +11,8 @@ from ser.data import train_dataloader, val_dataloader, test_dataloader
 from ser.params import Params, save_params
 from ser.transforms import transforms, normalize
 
+from ser.infer import do_infer
+
 main = typer.Typer()
 
 
@@ -59,7 +61,14 @@ def train(
 
 
 @main.command()
-def infer():
+def infer(
+    run_path: str = typer.Option(
+        ..., '-p', '--path', help="File path for inference."
+    ),
+    label: int = typer.Option(
+        6, '-l', '--label', help ="Label................"
+    )
+):
     run_path = Path("./path/to/one/of/your/training/runs")
     label = 6
 
@@ -82,22 +91,6 @@ def infer():
     print(f"This is a {pred}")
 
 
-def generate_ascii_art(pixels):
-    ascii_art = []
-    for row in pixels:
-        line = []
-        for pixel in row:
-            line.append(pixel_to_char(pixel))
-        ascii_art.append("".join(line))
-    return "\n".join(ascii_art)
 
 
-def pixel_to_char(pixel):
-    if pixel > 0.99:
-        return "O"
-    elif pixel > 0.9:
-        return "o"
-    elif pixel > 0:
-        return "."
-    else:
-        return " "
+
---
+++ ser/infer.py
@@ -0,0 +1,34 @@
+from ser.data import test_dataloader
+from ser.transforms import transforms, normalize
+
+def do_infer():
+
+
+
+
+def select_image(label):
+    dataloader = test_dataloader(1, transforms(normalize))
+    images, labels = next(iter(dataloader))
+    while labels[0].item() != label:
+        images, labels = next(iter(dataloader))
+    return images
+
+
+def generate_ascii_art(pixels):
+    ascii_art = []
+    for row in pixels:
+        line = []
+        for pixel in row:
+            line.append(pixel_to_char(pixel))
+        ascii_art.append("".join(line))
+    return "\n".join(ascii_art)
+
+def pixel_to_char(pixel):
+    if pixel > 0.99:
+        return "O"
+    elif pixel > 0.9:
+        return "o"
+    elif pixel > 0:
+        return "."
+    else:
+        return " "
